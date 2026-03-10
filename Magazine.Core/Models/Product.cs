b620b0c606bf5009ab798a486a423e85bbb96446
Valera <valerakochegin2005@gmail.com>
2026-03-11 00:48:39 +0300

    add new class Product

---
+++ Magazine.Core/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magazine.Core.Models
+{
+    internal class Product
+    {
+        public Guid Id { get; set; }
+        public string Definition { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; } // пусть будет тут путь до изображения
+    }
+}
