e6bac4c0d6044a7d5a0f3350061396bcd296a14e
Valera <valerakochegin2005@gmail.com>
2026-03-20 22:15:59 +0300

    описание методов в интерфейсе IProductService

--- Magazine.Core/Services/IProductService.cs
+++ Magazine.Core/Services/IProductService.cs
@@ -8,9 +8,32 @@ namespace Magazine.Core.Services
 {
     public interface IProductService
     {
+        /// <summary>
+        /// Добавляет новый продукт в систему
+        /// </summary>
+        /// <param name="product">Объект продукта, содержащий данные для добавления (название, цена, описание и т.д.)</param>
+        /// <returns>Добавленный продукт с присвоенным идентификатором</returns>
         Product add(Product product);
+
+        /// <summary>
+        /// Удаляет продукт из системы по его идентификатору
+        /// </summary>
+        /// <param name="id">Уникальный идентификатор (GUID) продукта, который необходимо удалить</param>
+        /// <returns>Удалённый продукт. Если продукт с указанным id не найден, возвращает null</returns>
         Product remove(Guid id);
+
+        /// <summary>
+        /// Редактирует существующий продукт
+        /// </summary>
+        /// <param name="product">Объект продукта с обновлёнными данными. Поле Id обязательно для идентификации редактируемого продукта</param>
+        /// <returns>Отредактированный продукт с применёнными изменениями</returns>
         Product edit(Product product);
+
+        /// <summary>
+        /// Выполняет поиск продукта по его идентификатору
+        /// </summary>
+        /// <param name="id">Уникальный идентификатор (GUID) продукта для поиска</param>
+        /// <returns>Найденный продукт, либо null если продукт с указанным id не существует</returns>
         Product? search(Guid id);
     }
 }
